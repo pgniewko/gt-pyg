1ad062eea0cb60afff2b9c1aadc3d7c33f736f61
Pawel Gniewek <gniewko.pablo@gmail.com>
2023-06-20 12:25:17 -0700

    Documenting nn modules

---
+++ gt_pyg/nn/mlp.py
@@ -0,0 +1,63 @@
+# Third party
+from torch import nn
+from torch import Tensor
+from torch_geometric.nn.resolver import activation_resolver
+from typing import List, Union, Optional, Dict, Any
+
+
+class MLP(nn.Module):
+    def __init__(
+        self,
+        input_dim: int,
+        output_dim: int,
+        hidden_dims: Union[int, List[int]],
+        num_hidden_layers: int = 1,
+        dropout: float = 0.0,
+        act: str = "relu",
+        act_kwargs: Optional[Dict[str, Any]] = None,
+    ):
+        """
+        Multi-Layer Perceptron (MLP) module.
+
+        Args:
+            input_dim (int): Dimensionality of the input features.
+            output_dim (int): Dimensionality of the output features.
+            hidden_dims (Union[int, List[int]]): Hidden layer dimensions.
+                If int, same hidden dimension is used for all layers.
+            num_hidden_layers (int, optional): Number of hidden layers. Default is 1.
+            dropout (float, optional): Dropout probability. Default is 0.0.
+            act (str, optional): Activation function name. Default is "relu".
+            act_kwargs (Dict[str, Any], optional): Additional arguments for the activation function.
+                                                   Default is None.
+        """
+        super(MLP, self).__init__()
+
+        if isinstance(hidden_dims, int):
+            hidden_dims = [hidden_dims] * num_hidden_layers
+
+        assert len(hidden_dims) == num_hidden_layers
+
+        hidden_dims = [input_dim] + hidden_dims
+        layers = []
+
+        for (i_dim, o_dim) in zip(hidden_dims[:-1], hidden_dims[1:]):
+            layers.append(nn.Linear(i_dim, o_dim, bias=True))
+            layers.append(activation_resolver(act, **(act_kwargs or {})))
+            if dropout > 0.0:
+                layers.append(nn.Dropout(p=dropout))
+
+        layers.append(nn.Linear(hidden_dims[-1], output_dim, bias=True))
+
+        self.mlp = nn.Sequential(*layers)
+
+    def forward(self, x: Tensor) -> Tensor:
+        """
+        Forward pass of the MLP module.
+
+        Args:
+            x (Any): Input tensor.
+
+        Returns:
+            Any: Output tensor.
+        """
+        return self.mlp(x)
