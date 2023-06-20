ed6f098bc872aff0acc874acf8e6c8f68c8e4b9f
Pawel Gniewek <gniewko.pablo@gmail.com>
2023-06-20 12:25:17 -0700

    Documenting nn modules

--- gt_pyg/data/utils.py
+++ gt_pyg/data/utils.py
@@ -1,20 +1,16 @@
-# import packages
-# general tools
+# Third party
 import numpy as np
-# RDkit
 from rdkit import Chem
 from rdkit.Chem.rdmolops import GetAdjacencyMatrix
-# Pytorch and Pytorch Geometric
 import torch
 from torch_geometric.data import Data
-from torch.utils.data import DataLoader
-
 from tdc.single_pred import ADME
 from rdkit import RDLogger
 from rdkit import rdBase
 
 
-__SMILES = 'c1ccccc1'
+__SMILES = "c1ccccc1"
+
 
 def clean_df(tdc_df, min_num_atoms=6):
     def count_fragments(mol):
@@ -27,20 +23,20 @@ def clean_df(tdc_df, min_num_atoms=6):
     for log_level in RDLogger._levels:
         rdBase.DisableLog(log_level)
 
-    tdc_df['mol'] = tdc_df.Drug.apply(Chem.MolFromSmiles)
-    tdc_df['num_frags'] = tdc_df.mol.apply(count_fragments)
-    tdc_df['num_atoms'] = tdc_df.mol.apply(count_atoms)
+    tdc_df["mol"] = tdc_df.Drug.apply(Chem.MolFromSmiles)
+    tdc_df["num_frags"] = tdc_df.mol.apply(count_fragments)
+    tdc_df["num_atoms"] = tdc_df.mol.apply(count_atoms)
     tdc_df = tdc_df.query("num_frags == 1").copy()
     tdc_df = tdc_df.query(f"num_atoms >= {min_num_atoms}").copy()
     return tdc_df
 
 
 def get_train_valid_test_data(endpoint, min_num_atoms=6):
-    data = ADME(name = endpoint)
+    data = ADME(name=endpoint)
     splits = data.get_split()
-    train_data = clean_df(splits['train'], min_num_atoms)
-    valid_data = clean_df(splits['valid'], min_num_atoms)
-    test_data = clean_df(splits['test'], min_num_atoms)
+    train_data = clean_df(splits["train"], min_num_atoms)
+    valid_data = clean_df(splits["valid"], min_num_atoms)
+    test_data = clean_df(splits["test"], min_num_atoms)
     return (train_data, valid_data, test_data)
 
 
@@ -53,7 +49,10 @@ def one_hot_encoding(x, permitted_list):
     if x not in permitted_list:
         x = permitted_list[-1]
 
-    binary_encoding = [int(boolean_value) for boolean_value in list(map(lambda s: x == s, permitted_list))]
+    binary_encoding = [
+        int(boolean_value)
+        for boolean_value in list(map(lambda s: x == s, permitted_list))
+    ]
 
     return binary_encoding
 
@@ -64,7 +63,7 @@ def get_pe(mol, pe_dim=6, normalized=True):
     degree = np.diag(np.sum(adj, axis=1))
     laplacian = degree - adj
     if normalized:
-        degree_inv_sqrt = np.diag(np.sum(adj, axis=1)**(-1/2))
+        degree_inv_sqrt = np.diag(np.sum(adj, axis=1) ** (-1.0 / 2.0))
         laplacian = degree_inv_sqrt @ laplacian @ degree_inv_sqrt
     try:
         val, vec = np.linalg.eig(laplacian)
@@ -72,80 +71,159 @@ def get_pe(mol, pe_dim=6, normalized=True):
         print(Chem.MolToSmiles(mol))
         raise
     vec = vec[:, np.argsort(val)]
-    return vec[:, 1:pe_dim + 1]
+    return vec[:, 1 : pe_dim + 1]
 
 
-def get_atom_features(atom, 
-                      use_chirality = True, 
-                      hydrogens_implicit = True):
+def get_atom_features(atom, use_chirality=True, hydrogens_implicit=True):
     """
     Takes an RDKit atom object as input and gives a 1d-numpy array of atom features as output.
     """
 
     # define list of permitted atoms
-    
-    permitted_list_of_atoms =  ['C','N','O','S','F','Si','P','Cl','Br','Mg','Na','Ca','Fe','As','Al','I', 'B','V','K','Tl','Yb','Sb','Sn','Ag','Pd','Co','Se','Ti','Zn', 'Li','Ge','Cu','Au','Ni','Cd','In','Mn','Zr','Cr','Pt','Hg','Pb','Unknown']
-    
-    if hydrogens_implicit == False:
-        permitted_list_of_atoms = ['H'] + permitted_list_of_atoms
-    
-    # compute atom features
-    
-    atom_type_enc = one_hot_encoding(str(atom.GetSymbol()), permitted_list_of_atoms)
-    
-    n_heavy_neighbors_enc = one_hot_encoding(int(atom.GetDegree()), [0, 1, 2, 3, 4, "MoreThanFour"])
-    
-    formal_charge_enc = one_hot_encoding(int(atom.GetFormalCharge()), [-3, -2, -1, 0, 1, 2, 3, "Extreme"])
-    
-    hybridisation_type_enc = one_hot_encoding(str(atom.GetHybridization()), ["S", "SP", "SP2", "SP3", "SP3D", "SP3D2", "OTHER"])
-    
-    is_in_a_ring_enc = [int(atom.IsInRing())]
-    
-    is_aromatic_enc = [int(atom.GetIsAromatic())]
-    
-    atomic_mass_scaled = [float((atom.GetMass() - 10.812)/116.092)]
-    
-    vdw_radius_scaled = [float((Chem.GetPeriodicTable().GetRvdw(atom.GetAtomicNum()) - 1.5)/0.6)]
-    
-    covalent_radius_scaled = [float((Chem.GetPeriodicTable().GetRcovalent(atom.GetAtomicNum()) - 0.64)/0.76)]
 
-    atom_feature_vector = atom_type_enc + n_heavy_neighbors_enc + formal_charge_enc + hybridisation_type_enc + is_in_a_ring_enc + is_aromatic_enc + atomic_mass_scaled + vdw_radius_scaled + covalent_radius_scaled
-                                    
+    permitted_list_of_atoms = [
+        "C",
+        "N",
+        "O",
+        "S",
+        "F",
+        "Si",
+        "P",
+        "Cl",
+        "Br",
+        "Mg",
+        "Na",
+        "Ca",
+        "Fe",
+        "As",
+        "Al",
+        "I",
+        "B",
+        "V",
+        "K",
+        "Tl",
+        "Yb",
+        "Sb",
+        "Sn",
+        "Ag",
+        "Pd",
+        "Co",
+        "Se",
+        "Ti",
+        "Zn",
+        "Li",
+        "Ge",
+        "Cu",
+        "Au",
+        "Ni",
+        "Cd",
+        "In",
+        "Mn",
+        "Zr",
+        "Cr",
+        "Pt",
+        "Hg",
+        "Pb",
+        "Unknown",
+    ]
+
+    if hydrogens_implicit == False:
+        permitted_list_of_atoms = ["H"] + permitted_list_of_atoms
+
+    # compute atom features
+
+    atom_type_enc = one_hot_encoding(str(atom.GetSymbol()), permitted_list_of_atoms)
+
+    n_heavy_neighbors_enc = one_hot_encoding(
+        int(atom.GetDegree()), [0, 1, 2, 3, 4, "MoreThanFour"]
+    )
+
+    formal_charge_enc = one_hot_encoding(
+        int(atom.GetFormalCharge()), [-3, -2, -1, 0, 1, 2, 3, "Extreme"]
+    )
+
+    hybridisation_type_enc = one_hot_encoding(
+        str(atom.GetHybridization()),
+        ["S", "SP", "SP2", "SP3", "SP3D", "SP3D2", "OTHER"],
+    )
+
+    is_in_a_ring_enc = [int(atom.IsInRing())]
+
+    is_aromatic_enc = [int(atom.GetIsAromatic())]
+
+    atomic_mass_scaled = [float((atom.GetMass() - 10.812) / 116.092)]
+
+    vdw_radius_scaled = [
+        float((Chem.GetPeriodicTable().GetRvdw(atom.GetAtomicNum()) - 1.5) / 0.6)
+    ]
+
+    covalent_radius_scaled = [
+        float((Chem.GetPeriodicTable().GetRcovalent(atom.GetAtomicNum()) - 0.64) / 0.76)
+    ]
+
+    atom_feature_vector = (
+        atom_type_enc
+        + n_heavy_neighbors_enc
+        + formal_charge_enc
+        + hybridisation_type_enc
+        + is_in_a_ring_enc
+        + is_aromatic_enc
+        + atomic_mass_scaled
+        + vdw_radius_scaled
+        + covalent_radius_scaled
+    )
+
     if use_chirality == True:
-        chirality_type_enc = one_hot_encoding(str(atom.GetChiralTag()), ["CHI_UNSPECIFIED", "CHI_TETRAHEDRAL_CW", "CHI_TETRAHEDRAL_CCW", "CHI_OTHER"])
+        chirality_type_enc = one_hot_encoding(
+            str(atom.GetChiralTag()),
+            [
+                "CHI_UNSPECIFIED",
+                "CHI_TETRAHEDRAL_CW",
+                "CHI_TETRAHEDRAL_CCW",
+                "CHI_OTHER",
+            ],
+        )
         atom_feature_vector += chirality_type_enc
-    
+
     if hydrogens_implicit == True:
-        n_hydrogens_enc = one_hot_encoding(int(atom.GetTotalNumHs()), [0, 1, 2, 3, 4, "MoreThanFour"])
+        n_hydrogens_enc = one_hot_encoding(
+            int(atom.GetTotalNumHs()), [0, 1, 2, 3, 4, "MoreThanFour"]
+        )
         atom_feature_vector += n_hydrogens_enc
 
     return np.array(atom_feature_vector)
 
 
-def get_bond_features(bond, 
-                      use_stereochemistry = True):
+def get_bond_features(bond, use_stereochemistry=True):
     """
     Takes an RDKit bond object as input and gives a 1d-numpy array of bond features as output.
     """
 
-    permitted_list_of_bond_types = [Chem.rdchem.BondType.SINGLE, Chem.rdchem.BondType.DOUBLE, Chem.rdchem.BondType.TRIPLE, Chem.rdchem.BondType.AROMATIC]
+    permitted_list_of_bond_types = [
+        Chem.rdchem.BondType.SINGLE,
+        Chem.rdchem.BondType.DOUBLE,
+        Chem.rdchem.BondType.TRIPLE,
+        Chem.rdchem.BondType.AROMATIC,
+    ]
 
     bond_type_enc = one_hot_encoding(bond.GetBondType(), permitted_list_of_bond_types)
-    
+
     bond_is_conj_enc = [int(bond.GetIsConjugated())]
-    
+
     bond_is_in_ring_enc = [int(bond.IsInRing())]
-    
+
     bond_feature_vector = bond_type_enc + bond_is_conj_enc + bond_is_in_ring_enc
-    
+
     if use_stereochemistry == True:
-        stereo_type_enc = one_hot_encoding(str(bond.GetStereo()), ["STEREOZ", "STEREOE", "STEREOANY", "STEREONONE"])
+        stereo_type_enc = one_hot_encoding(
+            str(bond.GetStereo()), ["STEREOZ", "STEREOE", "STEREOANY", "STEREONONE"]
+        )
         bond_feature_vector += stereo_type_enc
 
     return np.array(bond_feature_vector)
 
 
-#def create_pytorch_geometric_graph_data_list_from_smiles_and_labels(x_smiles, y):
+# def create_pytorch_geometric_graph_data_list_from_smiles_and_labels(x_smiles, y):
 def get_tensor_data(x_smiles, y, pe=True, pe_dim=6):
     """
     Inputs:
@@ -158,57 +236,60 @@ def get_tensor_data(x_smiles, y, pe=True, pe_dim=6):
     data_list = [G_1, G_2, ...] ... a list of torch_geometric.data.Data objects which represent labeled molecular graphs that can readily be used for machine learning
     
     """
-    
+
     data_list = []
-    
+
     for (smiles, y_val) in zip(x_smiles, y):
-        
+
         # convert SMILES to RDKit mol object
         mol = Chem.MolFromSmiles(smiles)
 
         # get feature dimensions
         n_nodes = mol.GetNumAtoms()
-        n_edges = 2*mol.GetNumBonds()
+        n_edges = 2 * mol.GetNumBonds()
         unrelated_smiles = "O=O"
         unrelated_mol = Chem.MolFromSmiles(unrelated_smiles)
         n_node_features = len(get_atom_features(unrelated_mol.GetAtomWithIdx(0)))
-        n_edge_features = len(get_bond_features(unrelated_mol.GetBondBetweenAtoms(0,1)))
+        n_edge_features = len(
+            get_bond_features(unrelated_mol.GetBondBetweenAtoms(0, 1))
+        )
 
         # construct node feature matrix X of shape (n_nodes, n_node_features)
         X = np.zeros((n_nodes, n_node_features))
 
         for atom in mol.GetAtoms():
             X[atom.GetIdx(), :] = get_atom_features(atom)
-            
+
         X = torch.tensor(X, dtype=torch.float)
-        
+
         # construct edge index array E of shape (2, n_edges)
         (rows, cols) = np.nonzero(GetAdjacencyMatrix(mol))
         torch_rows = torch.from_numpy(rows.astype(np.int64)).to(torch.long)
         torch_cols = torch.from_numpy(cols.astype(np.int64)).to(torch.long)
-        E = torch.stack([torch_rows, torch_cols], dim = 0)
-        
+        E = torch.stack([torch_rows, torch_cols], dim=0)
 
         # construct edge feature array EF of shape (n_edges, n_edge_features)
         EF = np.zeros((n_edges, n_edge_features))
-       
+
         if pe:
-            pe_numpy = get_pe(mol, pe_dim=pe_dim) 
+            pe_numpy = get_pe(mol, pe_dim=pe_dim)
             pe_tensor = torch.tensor(pe_numpy, dtype=torch.float)
         else:
             pe_tensor = None
 
-        for (k, (i,j)) in enumerate(zip(rows, cols)):
-            
-            EF[k] = get_bond_features(mol.GetBondBetweenAtoms(int(i),int(j)))
-        
-        EF = torch.tensor(EF, dtype = torch.float)
-        
+        for (k, (i, j)) in enumerate(zip(rows, cols)):
+
+            EF[k] = get_bond_features(mol.GetBondBetweenAtoms(int(i), int(j)))
+
+        EF = torch.tensor(EF, dtype=torch.float)
+
         # construct label tensor
-        y_tensor = torch.tensor(np.array([y_val]), dtype = torch.float)
-        
+        y_tensor = torch.tensor(np.array([y_val]), dtype=torch.float)
+
         # construct Pytorch Geometric data object and append to data list
-        data_list.append(Data(x=X, edge_index=E, edge_attr=EF, pe=pe_tensor, y=y_tensor))
+        data_list.append(
+            Data(x=X, edge_index=E, edge_attr=EF, pe=pe_tensor, y=y_tensor)
+        )
 
     return data_list
 
@@ -216,6 +297,7 @@ def get_tensor_data(x_smiles, y, pe=True, pe_dim=6):
 def get_node_dim():
     data = get_tensor_data([__SMILES], [0], pe=False)[0]
     return data.x.size(-1)
+
 
 def get_edge_dim():
     data = get_tensor_data([__SMILES], [0], pe=False)[0]
---
+++ gt_pyg/nn/gt_conv.py
@@ -0,0 +1,170 @@
+# Standard
+import math
+from typing import Optional
+
+# Third party
+from torch import nn
+from torch_geometric.nn import MessagePassing
+from torch_geometric.utils import softmax
+
+# GT-PyG
+from .mlp import MLP
+
+
+class GTConv(MessagePassing):
+    def __init__(
+        self,
+        node_in_dim: int,
+        hidden_dim: int,
+        edge_in_dim: Optional[int] = None,
+        num_heads: int = 1,
+        dropout: float = 0.0,
+        norm: str = "bn",
+        act: str = "relu",
+    ):
+        """
+        Graph Transformer Convolution (GTConv) module.
+
+        Args:
+            node_in_dim (int): Dimensionality of the input node features.
+            hidden_dim (int): Dimensionality of the hidden representations.
+            edge_in_dim (int, optional): Dimensionality of the input edge features.
+                                         Default is None.
+            num_heads (int, optional): Number of attention heads. Default is 1.
+            dropout (float, optional): Dropout probability. Default is 0.0.
+            norm (str, optional): Normalization type. Options: "bn" (BatchNorm), "ln" (LayerNorm).
+                                  Default is "bn".
+            act (str, optional): Activation function name. Default is "relu".
+        """
+        super(GTConv, self).__init__(node_dim=0, aggr="add")
+
+        assert hidden_dim % num_heads == 0
+        assert (edge_in_dim is None) or (edge_in_dim > 0)
+
+        self.WQ = nn.Linear(node_in_dim, hidden_dim, bias=True)
+        self.WK = nn.Linear(node_in_dim, hidden_dim, bias=True)
+        self.WV = nn.Linear(node_in_dim, hidden_dim, bias=True)
+        self.WO = nn.Linear(hidden_dim, node_in_dim, bias=True)
+
+        if edge_in_dim is not None:
+            assert node_in_dim == edge_in_dim
+            self.WE = nn.Linear(edge_in_dim, hidden_dim, bias=True)
+            self.WOe = nn.Linear(hidden_dim, edge_in_dim, bias=True)
+            self.ffn_e = MLP(
+                input_dim=edge_in_dim,
+                output_dim=edge_in_dim,
+                hidden_dims=hidden_dim,
+                num_hidden_layers=1,
+                dropout=dropout,
+                act=act,
+            )
+            if norm.lower() in ["bn", "batchnorm", "batch_norm"]:
+                self.norm1e = nn.BatchNorm1d(edge_in_dim)
+                self.norm2e = nn.BatchNorm1d(edge_in_dim)
+            elif norm.lower() in ["ln", "layernorm", "layer_norm"]:
+                self.norm1e = nn.LayerNorm(edge_in_dim)
+                self.norm2e = nn.LayerNorm(edge_in_dim)
+            else:
+                raise ValueError
+        else:
+            self.WE = self.register_parameter("WE", None)
+            self.WOe = self.register_parameter("WOe", None)
+            self.ffn_e = self.register_parameter("ffn_e", None)
+            self.norm1e = self.register_parameter("norm1e", None)
+            self.norm2e = self.register_parameter("norm2e", None)
+
+        if norm.lower() in ["bn", "batchnorm", "batch_norm"]:
+            self.norm1 = nn.BatchNorm1d(node_in_dim)
+            self.norm2 = nn.BatchNorm1d(node_in_dim)
+        elif norm.lower() in ["ln", "layernorm", "layer_norm"]:
+            self.norm1 = nn.LayerNorm(node_in_dim)
+            self.norm2 = nn.LayerNorm(node_in_dim)
+
+        self.dropout_layer = nn.Dropout(p=dropout)
+
+        self.ffn = MLP(
+            input_dim=node_in_dim,
+            output_dim=node_in_dim,
+            hidden_dims=hidden_dim,
+            num_hidden_layers=1,
+            dropout=dropout,
+            act=act,
+        )
+
+        self.num_heads = num_heads
+        self.node_in_dim = node_in_dim
+        self.edge_in_dim = edge_in_dim
+        self.hidden_dim = hidden_dim
+        self.dropout = dropout
+        self.norm = norm.lower()
+
+        self.reset_parameters()
+
+    def reset_parameters(self):
+        nn.init.xavier_uniform_(self.WQ.weight)
+        nn.init.xavier_uniform_(self.WK.weight)
+        nn.init.xavier_uniform_(self.WV.weight)
+        nn.init.xavier_uniform_(self.WO.weight)
+        if self.edge_in_dim is not None:
+            nn.init.xavier_uniform_(self.WE.weight)
+            nn.init.xavier_uniform_(self.WOe.weight)
+
+    def forward(self, x, edge_index, edge_attr=None):
+        x_ = x
+        Q = self.WQ(x).view(-1, self.num_heads, self.hidden_dim // self.num_heads)
+        K = self.WK(x).view(-1, self.num_heads, self.hidden_dim // self.num_heads)
+        V = self.WV(x).view(-1, self.num_heads, self.hidden_dim // self.num_heads)
+
+        out = self.propagate(edge_index, Q=Q, K=K, V=V, edge_attr=edge_attr, size=None)
+        out = out.view(-1, self.hidden_dim)
+
+        # NODES
+        out = self.dropout_layer(out)
+        out = self.WO(out) + x_  # Residual connection
+        out = self.norm1(out)
+        # FFN-NODES
+        ffn_in = out
+        out = self.ffn(out)
+        out = self.norm2(ffn_in + out)
+
+        if self.edge_in_dim is None:
+            out_eij = None
+        else:
+            out_eij = self._eij
+            self._eij = None
+            out_eij = out_eij.view(-1, self.hidden_dim)
+
+            # EDGES
+            out_eij_ = out_eij
+            out_eij = self.dropout_layer(out_eij)
+            out_eij = self.WOe(out_eij) + out_eij_  # Residual connection
+            out_eij = self.norm1e(out_eij)
+            # FFN-EDGES
+            ffn_eij_in = out_eij
+            out_eij = self.ffn_e(out_eij)
+            out_eij = self.norm2e(ffn_eij_in + out_eij)
+
+        return (out, out_eij)
+
+    def message(self, Q_i, K_j, V_j, index, edge_attr=None):
+        if self.WE is not None:
+            assert edge_attr is not None
+            E = self.WE(edge_attr).view(
+                -1, self.num_heads, self.hidden_dim // self.num_heads
+            )
+            K_j = E * K_j
+
+        d_k = Q_i.size(-1)
+        qijk = (Q_i * K_j).sum(dim=-1) / math.sqrt(d_k)
+        self._eij = (Q_i * K_j) / math.sqrt(d_k)
+        alpha = softmax(
+            qijk, index
+        )  # Log-Sum-Exp trick used. No need for clipping (-5,5)
+
+        return alpha.view(-1, self.num_heads, 1) * V_j
+
+    def __repr__(self) -> str:
+        return (
+            f"{self.__class__.__name__}({self.node_in_dim}, "
+            f"{self.hidden_dim}, heads={self.num_heads})"
+        )
