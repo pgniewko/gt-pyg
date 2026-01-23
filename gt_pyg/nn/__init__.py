97bb3e8cc856f22d433c243f15e59d95c7998eca
Pawel Gniewek <gniewko.pablo@gmail.com>
2026-01-22 19:53:03 -0800

    Revert __init__ exports

--- gt_pyg/nn/__init__.py
+++ gt_pyg/nn/__init__.py
@@ -1,17 +1,1 @@
-"""Neural network components for Graph Transformer.
 
-Classes:
-    GTConv: Graph Transformer convolution layer with attention mechanism.
-    MLP: Multi-layer perceptron module.
-    GraphTransformerNet: Full Graph Transformer network model.
-"""
-
-from .gt_conv import GTConv
-from .mlp import MLP
-from .model import GraphTransformerNet
-
-__all__ = [
-    "GTConv",
-    "MLP",
-    "GraphTransformerNet",
-]
